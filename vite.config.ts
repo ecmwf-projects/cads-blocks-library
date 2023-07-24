51f85ebcf63725d113ef26ded75d7fe3752444f2
sanchezi <sanchezi@predictia.es>
2023-07-24 11:19:48 +0200

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
-import { resolve } from 'node:path';
+import { resolve } from 'node:path'
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import EsLint from 'vite-plugin-linter'
+import typescript from '@rollup/plugin-typescript'
 
-import * as packageJson from './package.json';
+import * as packageJson from './package.json'
 
 const { EsLinter, linterPlugin } = EsLint
 
-
 // https://vitejs.dev/config/
-export default defineConfig(configEnv => ({
-  plugins: [react(),
-  dts({
-    include: ['src/**/*']
-  }),
-  linterPlugin({
-    include: ['./src}/**/*.{ts,tsx}'],
-    linters: [new EsLinter({ configEnv })],
-  }),],
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    dts({
+      include: ['src/**/*'],
+    }),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve('src', 'index.ts'),
@@ -29,11 +31,17 @@ export default defineConfig(configEnv => ({
       fileName: (format) => `cads-blocks-library.${format}.js`,
     },
     rollupOptions: {
-      treeshake: true,
       external: Object.keys(packageJson.peerDependencies),
       output: {
-        globals: (name) => name
-      }
+        globals: (name) => name,
+      },
+      plugins: [
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: 'dist',
+        }),
+      ],
     },
   },
-}));
+}))
