3a75c24a953da066df11041e480daaf8d2a1b7cc
pelusanchez <davidiglesanchez@gmail.com>
2023-11-22 08:41:11 +0100

    Use anchor due to issue.

--- src/components/blocks/LinkBlock.tsx
+++ src/components/blocks/LinkBlock.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Link from 'next/link'
+import Link, { LinkProps } from 'next/link'
 import styled from 'styled-components'
 import { ExternalLinkIcon } from '@radix-ui/react-icons'
 
@@ -8,6 +8,7 @@ import { LinkBlockInterface } from '../../models'
 
 export const LinkBlock = ({ block }: { block: LinkBlockInterface }) => {
   if (!block.href || !block.title) return null
+
   return (
     <BlockContent data-stylizable="block link-container">
       <CustomLink target={block['new-window'] ? '_blank' : '_self'} href={block.href} passHref>
@@ -19,13 +20,55 @@ export const LinkBlock = ({ block }: { block: LinkBlockInterface }) => {
   )
 }
 
+/**
+ * Due to https://github.com/vercel/next.js/issues/42157, we can not
+ * use the link component in some scenarios:
+ * - Only updating the hash
+ * - Only updating the query
+ */
+const CustomLink = (props: React.PropsWithChildren<{
+  href: LinkProps['href']
+  target: string
+  passHref: boolean
+}>) => {
+  // Check the href to see if it is a hash or query
+  // Check if href is a string or object
+  const isString = typeof props.href === 'string'
+  const isHash = isString && props.href.toString().startsWith('#')
+  const isQuery = isString && props.href.toString().startsWith('?')
+  const canUseLink = !isHash && !isQuery
+
+  if (canUseLink) {
+    return <CustomLinkWrapper {...props} />
+  }
+
+  // If we can't use the link component, then we need to
+  // use an anchor tag
+  const { href, children, target } = props
+
+  return (
+    <CustomAWrapper href={href.toString()} target={target}>
+      {children}
+    </CustomAWrapper>
+  )
+};
+
 const BlockContent = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1em;
 `
 
-const CustomLink = styled(Link)`
+const CustomLinkWrapper = styled(Link)`
+  display: inline;
+  position: relative;
+  svg {
+    position: relative;
+    margin-left: 0.625em;
+    bottom: -2px;
+  }
+`
+const CustomAWrapper = styled.a`
   display: inline;
   position: relative;
   svg {
